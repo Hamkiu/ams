e9aa5b92442b2b7318d1b9982ca4dcbee95781a2
Hamkiu <hamka@mbi.gov.my>
2026-09-23 16:11:49 +0800

    23-09-2026: tambah text

--- resources/views/auditadminreview/print.blade.php
+++ resources/views/auditadminreview/print.blade.php
@@ -231,7 +231,7 @@
                                 <tr>
 
                                     <th>
-                                        Jabatan
+                                        Jabatan / Unit
                                     </th>
 
                                     <td>
